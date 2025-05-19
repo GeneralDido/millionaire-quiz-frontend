573c98714ef81502362f6ce6519552f5d51bc196
GeneralDido <dimpanouris@gmail.com>
2025-05-19 16:29:28 +0200

    return GameList

--- hooks/useGamesList.ts
+++ hooks/useGamesList.ts
@@ -1,9 +1,16 @@
+// hooks/useGamesList.ts
 import {useQuery} from '@tanstack/react-query'
 import {get} from '@/utils/api'
 
+// Define the type for a game list entry
+export interface GameListEntry {
+  game_id: number
+  created_at: string
+}
+
 export function useGamesList() {
-  return useQuery<number[], Error>({
+  return useQuery<GameListEntry[], Error>({
     queryKey: ['games-list'],
-    queryFn: () => get<number[]>('/games/list')
+    queryFn: () => get<GameListEntry[]>('/games/list')
   })
 }
