c3644765e613e7abdb194aafecacdcdbbc18a92a
GeneralDido <dimpanouris@gmail.com>
2025-05-22 12:24:06 +0200

    add favicon.svg

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,15 +3,15 @@ import type {Metadata} from 'next'
 export const metadata: Metadata = {
   icons: {
     icon: [
-      '/favicon-16x16.png',
-      '/favicon-32x32.png',
-      '/favicon.ico',
-      {url: '/favicon.svg', sizes: 'any', type: 'image/svg+xml'}
+      {url: '/favicon.ico', sizes: '32x32'},
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      {url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+      {url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png'}
     ],
-
     shortcut: '/favicon.ico',
-    apple: '/apple-touch-icon.png',
-
+    apple: [
+      {url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png'}
+    ],
     other: [
       {
         rel: 'icon',
